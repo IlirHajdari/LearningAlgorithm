35b87297ef54a19365c94f41d29de943a1e39713
Ilir Hajdari <ilir.hajdari111@gmail.com>
2022-05-06 02:55:56 -0400

    Create learnAlgo.cpp

---
+++ learnAlgo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+vector<float> X 
+{
+  5.1, 3.5, 1.4, 0.2
+  4.9, 3.0, 1.4, 0.2
+  6.2, 3.4, 5.4, 2.3
+  5.9, 3.0, 5.1, 1.8
+
+};
+
+vector<float> y 
+{
+  0,
+  0,
+  1,
+  1
+};
+
